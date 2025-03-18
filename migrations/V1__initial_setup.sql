3ebd22ed704afbc9e4cd0cda83a8c27e3150e7b3
Jesse <magicjedi90@aim.com>
2025-03-18 08:07:41 -0700

    Convert changelog format to YAML and add projects table.
    Replaced the XML-based changelog with a YAML-based structure for better readability and maintainability. Introduced a `projects` table with columns `id`, `name`, and `job_id` in the schema. Updated `.gitignore` to include `liquibase.properties`.

---
+++ migrations/V1__initial_setup.sql
@@ -0,0 +1,41 @@
+CREATE TYPE proficiency AS ENUM ('Getting Started', 'Proficient', 'Expert');
+
+create table skills
+(
+    id                serial
+        constraint skills_pk
+            primary key,
+    name              varchar(250),
+    description       varchar(2500),
+    official_site_url varchar(2048),
+    proficiency       proficiency
+);
+
+create table jobs
+(
+    id serial
+        constraint jobs_pk
+            primary key,
+    start_date date,
+    end_date date,
+    is_current_job bool,
+    company_name varchar(250),
+    company_website varchar(2048),
+    description varchar(2500),
+    roles varchar(2500),
+    responsibilities varchar(2500)
+);
+
+create table projects
+(
+    id          serial
+        constraint projects_pk
+            primary key,
+    name        varchar(100),
+    description varchar(2500),
+    github_url varchar(2048),
+    job_id      integer,
+    constraint projects_jobs_id_fk
+        foreign key(job_id) references jobs(id)
+);
+
