67e65130bfad31932ba24f5b55bb3d496ce39399
Jesse <magicjedi90@aim.com>
2025-03-20 07:21:17 -0700

    Refactor and expand SQL schema with new relationships
    Aligned column formatting for better readability and maintainability. Added a `projects_skills` table to establish many-to-many relationships between `projects` and `skills`, including cascading deletes for related records. Enhanced foreign key constraints for data integrity.

--- migrations/V1__initial_setup.sql
+++ migrations/V1__initial_setup.sql
@@ -13,16 +13,16 @@ create table skills
 
 create table jobs
 (
-    id serial
+    id               serial
         constraint jobs_pk
             primary key,
-    start_date date,
-    end_date date,
-    is_current_job bool,
-    company_name varchar(250),
-    company_website varchar(2048),
-    description varchar(2500),
-    roles varchar(2500),
+    start_date       date,
+    end_date         date,
+    is_current_job   bool,
+    company_name     varchar(250),
+    company_website  varchar(2048),
+    description      varchar(2500),
+    roles            varchar(2500),
     responsibilities varchar(2500)
 );
 
@@ -33,9 +33,24 @@ create table projects
             primary key,
     name        varchar(100),
     description varchar(2500),
-    github_url varchar(2048),
+    github_url  varchar(2048),
     job_id      integer,
     constraint projects_jobs_id_fk
-        foreign key(job_id) references jobs(id)
+        foreign key (job_id) references jobs (id)
+            on delete cascade
 );
 
+create table projects_skills
+(
+    id         serial
+        constraint projects_skills_pk
+            primary key,
+    project_id integer
+        constraint projects_skills_projects_id_fk
+            references projects (id)
+            on delete cascade,
+    skill_id   integer
+        constraint projects_skills_skills_id_fk
+            references skills (id)
+            on delete cascade
+);
